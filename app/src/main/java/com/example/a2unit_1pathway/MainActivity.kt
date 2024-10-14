9d90b12ac6cd934e6f148f45228b6582f93a46a0
TheShamRio <xsham@inbox.ru>
2024-10-14 13:57:54 +0300

    use a when expression instead of a when statement

--- app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
+++ app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
@@ -18,28 +18,29 @@ import com.example.a2unit_1pathway.ui.theme._2unit_1pathwayTheme
 //}
 
 //------------------------------------------------
+//fun main() {
+//    val trafficLightColor = "Black"
+
+//    val message =
+//        if (trafficLightColor == "Red") "Stop"
+//        else if (trafficLightColor == "Yellow") "Slow"
+//        else if (trafficLightColor == "Green") "Go"
+//else "Invalid traffic-light color"
+
+//    println(message)
+//}
+ //-----------------------------------------------
 fun main() {
     val trafficLightColor = "Black"
 
-    val message =
-        if (trafficLightColor == "Red") "Stop"
-        else if (trafficLightColor == "Yellow") "Slow"
-        else if (trafficLightColor == "Green") "Go"
-        else "Invalid traffic-light color"
-
-    println(message)
+    val message = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+     println(message)
 }
- //-----------------------------------------------
-//fun main() {
-//    val trafficLightColor = "Amber"
-
-//    when (trafficLightColor) {
-//        "Red" -> println("Stop")
-//        "Yellow", "Amber" -> println("Slow")
-//        "Green" -> println("Go")
-//        else -> println("Invalid traffic-light color")
-//    }
-//}
 //-------------------------------------------------
 
 //fun main() {
