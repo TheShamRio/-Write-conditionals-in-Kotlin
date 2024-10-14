d1f4df5fc8ca4d52fcfeca97a327619c69ac4edc
TheShamRio <xsham@inbox.ru>
2024-10-14 13:54:09 +0300

    TrassicLight add a message

--- app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
+++ app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
@@ -18,40 +18,41 @@ import com.example.a2unit_1pathway.ui.theme._2unit_1pathwayTheme
 //}
 
 //------------------------------------------------
-//fun main() {
-//    val trafficLightColor = "Black"
-//
-//if (trafficLightColor == "Red") {
-//        println("Stop")
-//   } else if (trafficLightColor == "Yellow") {
-//        println("Slow")
-//    } else if (trafficLightColor == "Green") {
-///        println("Go")
-//    } else {
-//        println("Invalid traffic-light color")
-//    }
+fun main() {
+    val trafficLightColor = "Black"
 
-//}
+    val message = if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}
  //-----------------------------------------------
 //fun main() {
-//    val trafficLightColor = "Yellow"
+//    val trafficLightColor = "Amber"
 
 //    when (trafficLightColor) {
 //        "Red" -> println("Stop")
-//        "Yellow" -> println("Slow")
+//        "Yellow", "Amber" -> println("Slow")
 //        "Green" -> println("Go")
 //        else -> println("Invalid traffic-light color")
- //   }
+//    }
 //}
 //-------------------------------------------------
 
-fun main() {
-    val x: Any = 20
+//fun main() {
+//    val x: Any = 20
 
-    when (x) {
-        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
-        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
-        is Int -> println("x is an integer number, but not between 1 and 10.")
-        else -> println("x isn't an integer number.")
-    }
-}
+//    when (x) {
+//       2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+//        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+//        is Int -> println("x is an integer number, but not between 1 and 10.")
+//        else -> println("x isn't an integer number.")
+//    }
+//}
+//-----------------------------------------------------
