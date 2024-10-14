388892b7b435bf8c699daf9bb8a4fb056ef0c3a1
TheShamRio <xsham@inbox.ru>
2024-10-14 13:55:48 +0300

    Add a println() statement to the end of the program and then pass it the message variable as an argument

--- app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
+++ app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
@@ -21,16 +21,13 @@ import com.example.a2unit_1pathway.ui.theme._2unit_1pathwayTheme
 fun main() {
     val trafficLightColor = "Black"
 
-    val message = if (trafficLightColor == "Red") {
-        println("Stop")
-    } else if (trafficLightColor == "Yellow") {
-        println("Slow")
-    } else if (trafficLightColor == "Green") {
-        println("Go")
-    } else {
-        println("Invalid traffic-light color")
-    }
+    val message =
+        if (trafficLightColor == "Red") "Stop"
+        else if (trafficLightColor == "Yellow") "Slow"
+        else if (trafficLightColor == "Green") "Go"
+        else "Invalid traffic-light color"
 
+    println(message)
 }
  //-----------------------------------------------
 //fun main() {
