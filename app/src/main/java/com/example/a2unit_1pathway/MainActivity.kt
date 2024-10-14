6863aeed156aa09c2f71351519a9b2a2112a0837
TheShamRio <xsham@inbox.ru>
2024-10-14 13:46:11 +0300

    Using "in"

--- app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
+++ app/src/main/java/com/example/a2unit_1pathway/MainActivity.kt
@@ -46,10 +46,11 @@ import com.example.a2unit_1pathway.ui.theme._2unit_1pathwayTheme
 //-------------------------------------------------
 
 fun main() {
-    val x = 3
+    val x = 4
 
     when (x) {
         2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
         else -> println("x isn't a prime number between 1 and 10.")
     }
 }
